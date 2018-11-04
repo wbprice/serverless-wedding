2759a7e2ceb8e6e94538e1958245a8da3a5f960b
Blaine Price <1wbprice@gmail.com>
2018-11-04 11:44:49 -0500

    serde into a create RSVP struct

--- serverless-wedding-api/src/lib.rs
+++ serverless-wedding-api/src/lib.rs
@@ -1,13 +1,19 @@
 #[macro_use] extern crate cpython;
 #[macro_use] extern crate lando;
+#[macro_use] extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
 
 use lando::Response;
+use serde_json::{Value, Error};
 
 mod models;
 
 gateway! {
     "create" => |request, _context| {
-        print!("Hey CloudFront, this is {:?}", request);
+        let rsvp : models::CreateRSVP = serde_json::from_slice(request.body()).unwrap();
+        println!("{:#?}", rsvp);
         Ok(Response::new("create"))
     },
 
--- serverless-wedding-api/src/models.rs
+++ serverless-wedding-api/src/models.rs
@@ -1,12 +1,17 @@
-pub struct RSVP { 
+#[derive(Deserialize, Debug)]
+pub struct RSVP {
     household_id: String,
     id: String,
     name: String,
     salutation: String,
     email_address: String,
     attending: bool,
-    dietary_restrictions: String,
-    entree_selection: String,
     invitation_submitted: bool,
     reminder_submitted: bool
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CreateRSVP {
+    name: String,
+    email_address: String
 }
