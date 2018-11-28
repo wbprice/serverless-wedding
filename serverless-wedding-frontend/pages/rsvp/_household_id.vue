afc5407a67525f9a41a13cde28709bfd3a99b0ee
Blaine Price <1wbprice@gmail.com>
2018-11-27 21:40:50 -0500

    add missing stuff

---
+++ serverless-wedding-frontend/pages/rsvp/_household_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="rsvp">
+    <h1>RSVP</h1>
+    <p>{{ $route.params.household_id }}</p>
+    <div class="field">
+      <b-switch 
+        type="is-info" 
+        size="is-large">Will You Be There?</b-switch>
+    </div>
+    <div class="field">
+      <button class="button is-primary is-large">Send</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hello: 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
