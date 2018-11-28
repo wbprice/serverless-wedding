3d333a2ef8b6fee4b05cdffd335f08bd72cb29b6
Blaine Price <1wbprice@gmail.com>
2018-11-27 20:42:21 -0500

    Add nuxt stuff

---
+++ serverless-wedding-frontend/layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+:root {
+  --salmon: rgba(250, 128, 114, 1);
+  --red: rgba(179, 0, 27, 1);
+  --yellow: rgba(255, 231, 76, 1);
+  --white: rgba(255, 255, 255, 1);
+  --slate: rgba(98, 131, 149, 1);
+  --darkslate: rgba(98, 131, 149, 1);
+}
+
+body {
+  background-color: var(--salmon);
+  color: var(--white);
+  text-align: center;
+  font-family: 'Josefin Sans', sans-serif;
+  font-display: auto;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: 'Abril Fatface', cursive;
+  font-display: auto;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 960px;
+}
+</style>
---
+++ serverless-wedding-frontend/pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="title container">
+    <section class="rings">
+      <div class="ring his" />
+      <div class="ring hers" />
+    </section>
+
+    <h1>
+      <span class="underline">Blaine</span>
+      <span class="body-type"> is getting married to </span>
+      <span class="underline">Ling Ling</span>
+      <span class="date">July 6<sup>th</sup>, 2019</span>
+    </h1>
+
+    <section class="location">
+      <p>Somewhere around Denver</p>
+    </section> 
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.title {
+  padding-top: 1em;
+  font-size: 5em;
+  line-height: 1.25em;
+  color: var(--white);
+}
+
+.rings {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 0.5em;
+}
+
+.ring {
+  width: 1em;
+  height: 1em;
+  border: 0.1em solid var(--darkslate);
+  box-shadow: inset 0 0 0em 0.03em var(--white);
+  border-radius: 100%;
+}
+
+.ring.his {
+  margin-right: -0.25em;
+  margin-bottom: -0.25em;
+}
+
+.ring.hers {
+  margin-left: -0.25em;
+  margin-top: -0.25em;
+}
+
+.date {
+  margin-top: 0.25em;
+  color: var(--darkslate);
+  display: block;
+}
+
+.location {
+  margin-top: 1em;
+  font-size: 2rem;
+}
+
+sup {
+  font-size: 0.5em;
+}
+</style>
