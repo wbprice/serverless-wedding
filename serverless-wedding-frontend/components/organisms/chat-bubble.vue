03b1aed7345982d690d941dd2ddf680308e7efa0
Blaine Price <1wbprice@gmail.com>
2018-12-15 13:16:17 -0500

    Add speech bubble components and page.

---
+++ serverless-wedding-frontend/components/organisms/chat-bubble.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="chat-bubble">
+    <div class="chat-bubble-metadata">
+      {{ heading }}
+    </div>
+    <div class="chat-bubble-content">
+      {{ content }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatBubble',
+  props: {
+    heading: {
+      type: String,
+      default: 'Person Name'
+    },
+    content: {
+      type: String,
+      default: 'Sample Text'
+    }
+  }
+}
+</script>
+
+<style>
+.chat-bubble {
+  background: var(--white);
+  margin: 0.5em 0;
+  max-width: 75%;
+  border-radius: 1em;
+  padding: 0.75em;
+}
+
+.chat-bubble-blaine {
+  text-align: left;
+}
+
+.chat-bubble-ling {
+  text-align: right;
+  align-self: flex-end;
+}
+</style>
