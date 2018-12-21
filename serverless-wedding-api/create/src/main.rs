84fe5a76c0d6b44ce7cd5700045add50b0a9ea92
Blaine Price <1wbprice@gmail.com>
2018-12-21 09:58:06 -0500

    Add create and read crates

---
+++ serverless-wedding-api/create/src/main.rs
@@ -0,0 +1,52 @@
+#[macro_use]
+extern crate serde_derive;
+
+use lambda_http::{lambda, IntoResponse, Request};
+use lambda_runtime::{error::HandlerError, Context};
+
+use serde;
+use serde_json::json;
+use serde_dynamodb;
+use uuid;
+use rusoto_core;
+use rusoto_dynamodb;
+
+mod rsvp;
+
+fn main() {
+    lambda!(read_handler)
+}
+
+fn read_handler(
+    request: Request,
+    context: Context,
+) -> Result<impl IntoResponse, HandlerError> {
+
+    let payload : rsvp::NewRSVP = serde_json::from_slice(request.body()).unwrap_or_else(|error| {
+        panic!("there was an error deserializing the request! {:?}", error);
+    });
+
+    let rsvp = rsvp::create_rsvp_record(payload).unwrap_or_else(|_| {
+        panic!("Error at the disco");
+    });
+
+    return Ok(json!(rsvp));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_handler() {
+        let request = Request::default();
+        let expected = json!({
+        "message": "Go Serverless v1.0! Your function executed successfully!"
+        })
+        .into_response();
+        let response = handler(request, Context::default())
+            .expect("expected Ok(_) value")
+            .into_response();
+        assert_eq!(response.body(), expected.body())
+    }
+}
