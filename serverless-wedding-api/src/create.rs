c7600e2cca98e00488b2a7c82dd3610b9b1c4590
Blaine Price <1wbprice@gmail.com>
2019-01-04 20:45:29 -0500

    workable

--- serverless-wedding-api/src/create.rs
+++ serverless-wedding-api/src/create.rs
@@ -3,6 +3,9 @@ use lambda_runtime::{error::HandlerError, Context};
 use serde_json::{{json}};
 use std::ops::Deref;
 
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, PutItemInput, PutItemError};
+use serde_dynamodb;
+
 mod rsvp;
 
 fn main() {
@@ -13,15 +16,41 @@ fn handler(
     request: Request,
     _: Context,
 ) -> Result<impl IntoResponse, HandlerError> {
-    let body = request.body().deref();
-    let new_rsvp: rsvp::NewRSVP = serde_json::from_slice(body).unwrap_or_else(|err| {
+    let body_slice = request.body().deref();
+    let new_rsvp: rsvp::NewRSVP = serde_json::from_slice(body_slice).unwrap_or_else(|err| {
         println!("error: {:?}", err);
         panic!("oh no");
     });
 
-    let rsvp = rsvp::RSVP::new(new_rsvp);
+    match create_rsvp_record(new_rsvp) {
+        Ok(rsvp) => {
+            Ok(json!(rsvp))
+        },
+        Err(error) => {
+            Ok(json!({message: "Error"}))
+        }
+    }
+}
 
-    Ok(json!(rsvp))
+pub fn create_rsvp_record(new_rsvp: NewRSVP) -> Result<RSVP, PutItemError>{
+    let rsvp = rsvp::RSVP::new(new_rsvp);
+    let rsvp : RSVP = create_rsvp(new_rsvp);
+    let client = DynamoDbClient::new(Region::UsEast1);
+
+    let input = PutItemInput {
+        item: serde_dynamodb::to_hashmap(&rsvp).unwrap(),
+        table_name: env::var("RSVP_TABLE_NAME").unwrap(),
+        ..PutItemInput::default()
+    };
+    
+    match client.put_item(input).sync() {
+        Ok(_) => {
+            return Ok(rsvp);
+        },
+        Err(err) => {
+            return Err(err);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -35,6 +64,7 @@ mod tests {
             "message": "Go Serverless v1.0! Your function executed successfully!"
         })
         .into_response();
+
         let response = handler(request, Context::default())
             .expect("expected Ok(_) value")
             .into_response();
