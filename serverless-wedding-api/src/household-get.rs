0b7f591ea3874030cb33084bbc1b21372a817f0d
Blaine Price <1wbprice@gmail.com>
2019-01-12 20:36:54 -0500

    Create a GET /household route that accepts a UUID parameter

--- serverless-wedding-api/src/household-get.rs
+++ serverless-wedding-api/src/household-get.rs
@@ -5,7 +5,9 @@ use lambda_http::{lambda, IntoResponse, Request, Body};
 use lambda_runtime::{error::HandlerError, Context};
 use serde_json::{json};
 use std::ops::Deref;
+use url::{Url, ParseError};
 use log::{info, error};
+use uuid::Uuid;
 
 mod rsvp;
 
@@ -18,17 +20,21 @@ fn handler(
     request: Request,
     _: Context,
 ) -> Result<impl IntoResponse, HandlerError> {
-    let body = request.body().deref();
-    let person: rsvp::Person = serde_json::from_slice(body_slice).unwrap();
+    let uri = Url::parse(&request.uri().to_string()).unwrap_or_else(|_| {
+        panic!("Couldn't parse uri");
+    });
 
-    match rsvp::RSVP::create_record(person) {
-        Ok(rsvp) => {
-            Ok(json!(rsvp))
-        },
-        Err(_errcar) => {
-            Ok(json!({"message": "Error"}))
-        }
-    }
+    let path_segments = uri.path_segments().map(|c| c.collect::<Vec<_>>()).unwrap_or_else(|| {
+        panic!("uri didn't contain segments");
+    });
+    
+    let uuid = Uuid::parse_str(&path_segments[1].to_string()).unwrap_or_else(|_| {
+        panic!("UUID not provided");
+    });
+
+    println!("{:?}", uuid);
+
+    Ok(json!({"message": "you go girl"}))
 }
 
 #[cfg(test)]
@@ -38,12 +44,9 @@ mod tests {
     #[test]
     fn create_handler_handles() {
 
-        let payload = r#"{
-            "email_address": "example@email.com",
-            "name": "Blaine Price"
-        }"#;
-
-        let request = Request::new(Body::from(payload));
+        let mut request = Request::new(Body::default());
+        *request.uri_mut() = "https://api.slswedding.com/household/f2de1c2b-b6e2-4caa-a5b2-fd7fe8ba7efe".parse().unwrap();
+        println!("{:?}", request);
 
         handler(request, Context::default()).expect("expected Ok(_) value");
     }
