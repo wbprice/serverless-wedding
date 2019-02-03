512a6f833b19b3f33d4f422a73e7cac7e3c098ad
Blaine Price <1wbprice@gmail.com>
2019-02-03 11:51:51 -0500

    Add missing get

---
+++ serverless-wedding-api/src/rsvp-get.rs
@@ -0,0 +1,57 @@
+extern crate log;
+extern crate simple_logger;
+
+use lambda_http::{lambda, IntoResponse, Request, RequestExt};
+use lambda_runtime::{error::HandlerError, Context};
+use serde_json::json;
+use log::{info, error};
+use uuid::Uuid;
+
+mod rsvp;
+use crate::rsvp::RSVP;
+
+fn main() {
+    simple_logger::init_with_level(log::Level::Info).unwrap();
+    lambda!(handler)
+}
+
+fn handler(
+    request: Request,
+    _: Context,
+) -> Result<impl IntoResponse, HandlerError> {
+    let path_parameters = request.path_parameters();
+    let uuid : Uuid = Uuid::parse_str(
+        path_parameters.get("id").unwrap()
+    ).unwrap();
+
+    match RSVP::get(uuid) {
+        Ok(response) => Ok(json!(response)),
+        Err(_err) => Ok(json!({"message": "something bad happened!"}))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lambda_http::{http, Body};
+
+    #[test]
+    #[ignore]
+    fn patch_handler_handles() {
+
+        let payload = r#"{
+            "attending": true,
+            "invitation_submitted": true,
+            "reminder_submitted": true
+        }"#;
+
+        let request = http::Request::builder()
+             .uri("https://api.com/")
+             .method("PUT")
+             .header("Content-Type", "application/json")
+             .body(Body::from(payload.clone()))
+             .expect("failed to build request");
+
+        handler(request, Context::default()).expect("Expected an OK response");
+    }
+}
