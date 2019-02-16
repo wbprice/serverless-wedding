7ef08f0962ed77ce581b45d6537ddfed09ee8479
Blaine Price <1wbprice@gmail.com>
2019-02-15 23:36:09 -0700

    add images for rsvp cases

--- serverless-wedding-frontend/pages/rsvp/_household_id/complete.vue
+++ serverless-wedding-frontend/pages/rsvp/_household_id/complete.vue
@@ -1,16 +1,25 @@
 <template>
   <section class="rsvp-complete container">
-    <h1>RSVP Completed</h1>
+    <h1>RSVP</h1>
 
     <div 
       v-if="going"
       class="going">
+      <img
+        alt="A paper plane being thrown"
+        class="big icon"
+        src="~/assets/paper-plane.png">
       <h2>We'll see you there!</h2>
+      <p>Check the logistics page for more information!</p>
     </div>
 
     <div 
       v-else
       class="not-going">
+      <img
+        alt="A present"
+        class="big icon"
+        src="~/assets/money.png">
       <h2>Sorry you can't make it!</h2>
       <p>There's always the registry!</p>
     </div>
