37ca937b11a968042644a5022164c1124000bc12
Blaine Price <1wbprice@gmail.com>
2019-02-16 11:27:06 -0700

    Add skeleton dep

--- serverless-wedding-frontend/nuxt.config.js
+++ serverless-wedding-frontend/nuxt.config.js
@@ -13,7 +13,13 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: []
+    link: [
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css'
+      }
+    ]
   },
 
   /*
