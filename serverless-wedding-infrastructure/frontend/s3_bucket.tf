01d96515867f9a112a809f0d9ddd8a93160bf7f9
Blaine Price <1wbprice@gmail.com>
2019-02-20 22:44:56 -0500

    Fix error document

--- serverless-wedding-infrastructure/frontend/s3_bucket.tf
+++ serverless-wedding-infrastructure/frontend/s3_bucket.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "blinging_love_bucket" {
 
   website {
     index_document = "index.html"
-    error_document = "404.html"
+    error_document = "index.html"
   }
 
   policy = "${data.template_file.bucket_access_policy.rendered}"
