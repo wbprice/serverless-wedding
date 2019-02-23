5418130b3bec3e5e3d0e667f4d83d7ae1dc7715a
Blaine Price <1wbprice@gmail.com>
2019-02-22 19:40:27 -0500

    Proofread registry.

--- serverless-wedding-frontend/pages/registry.vue
+++ serverless-wedding-frontend/pages/registry.vue
@@ -4,18 +4,18 @@
       <h1>Registry</h1>
 
       <section class="poem">
-        <p>Our life together has begun
-        We have almost everything under the sun.
-        Your presence would be a gift
-        But if you insist on giving us a lift
-        We would like to spread our adventurous wings
-        With the help of some honeymoon bling
-        Our holiday will surely be a dream come true
-        All from the THANKS of YOU!</p>
+        <p>Our life together has begun.<br>
+          We have almost everything under the sun.<br>
+          Your presence would be a gift.<br>
+          But if you insist on giving us a lift,<br>
+          we would like to spread our adventurous wings<br>
+          With the help of some honeymoon bling<br>
+          Our holiday will surely be a dream come true<br>
+          All thanks to you!</p><br>
 
-        <p>For those of who can’t come to town
-        Please don’t let that get you down
-        We will celebrate round two with you!</p>
+        <p>For those of who can’t come to town<br>
+          Please don’t let that get you down<br>
+          We will celebrate round two with you!</p>
       </section>
 
       <Card
@@ -47,8 +47,7 @@ export default {
       registry: [
         {
           title: 'Accomodations',
-          content: `What better place to get a good nights rest in a rustic cabin next to the moutains!
-          We will definitely appreciate your generosity when we crash in after a long day of hiking!`,
+          content: `Is there a better place to get a good night's rest than in a cabin in the mountains? We will appreciate your generosity when we crash after a long day of hiking!`,
           cost: '129.99',
           buttonText: 'Buy It',
           image: '/lodging.jpg'
@@ -56,56 +55,49 @@ export default {
         {
           title: 'Roundtrip Air Fare',
           cost: '218.88',
-          content: `As much as we love traveling, airfare is an inevitable and large expense of our trip.
-          Fly us to the wedding, honeymoon, and back home.  We will definitely appreciate your generosity when we land safely at our destination!`,
+          content: `We love traveling, but airfare is expensive. Fly us to the wedding, to the honeymoon, and back home.  We will appreciate your generosity when we land safely at our destination!`,
           buttonText: 'Buy It',
           image: '/airfare.jpg'
         },
         {
           title: 'Sightseeing Tours',
           cost: '78.88',
-          content: `Spending the afternoon seeing the sights and being true tourists.
-          We will definitely appreciate your generosity when we are exploring all the sights!`,
+          content: `We'll spend the afternoon seeing the sights and being tourists. We will appreciate your generosity when we are exploring all the sights!`,
           buttonText: 'Buy It',
           image: '/sightseeing.jpg'
         },
         {
           title: 'Romantic Dinner',
           cost: '99.99',
-          content: `Dinner and drinks to refuel after a day of hiking.
-          We will definitley appreciate your generosity when we are enjoying the good foods like a true foodie should!`,
+          content: `Help us buy dinner and drinks to refuel after a day of hiking. We'll appreciate your generosity when we are enjoying good food as a true foodie should!`,
           buttonText: 'Buy It',
           image: '/dining.jpg'
         },
         {
           title: 'Adventure Package',
           cost: '168.88',
-          content: `To fufill the adventurous spirits within.
-          We will definitley appreciate your generosity when we are soaring amongst the birds!`,
+          content: `This package will help us to satisfy our adventurous spirits. We will appreciate your generosity when we are soaring amongst the birds!`,
           buttonText: 'Buy It',
           image: '/parasailing.jpg'
         },
         {
           title: 'Spa for Two',
           cost: '68.88',
-          content: `Massage and relaxation would be a wonderful way of recovery after some long hikes.
-          We will definitley appreciate your generosity when I am one with Jello.`,
+          content: `Getting a massage will be a wonderful way to recover after a long hike. We will appreciate your generosity when we are one with Jello.`,
           buttonText: 'Buy It',
           image: '/spa.jpg'
         },
         {
           title: 'Horseback Riding',
           cost: '88.88',
-          content: `Horseback ride on some beautiful trails.  We will definitley appreciate your generosity when we are
-          soaking in the beautiful weather and views.`,
+          content: `Help us buy a horseback ride on beautiful trails. We will appreciate your generosity when we are soaking in the beautiful weather and views.`,
           buttonText: 'Buy It',
           image: '/horseback-riding.jpg'
         },
         {
           title: 'Dancing',
           cost: '29.99',
-          content: `Who doesn't like to boogie woogie woogie?
-          We will definitley appreciate your generosity when the music makes us groovy.`,
+          content: `Who doesn't like to boogie-woogie? We will appreciate your generosity when the music makes us groovy.`,
           buttonText: 'Buy It',
           image: '/dance.jpg'
         },
