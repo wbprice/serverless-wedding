288100ba8af0ecece8148aaff896275674e1147d
Blaine Price <1wbprice@gmail.com>
2019-02-23 22:26:15 -0500

    stuff

--- serverless-wedding-frontend/pages/logistics.vue
+++ serverless-wedding-frontend/pages/logistics.vue
@@ -9,18 +9,35 @@
     </Card>
 
     <Card>
-      <h2>Rehearsal Afternoon</h2>
-      <p>TBD</p>
+      <h2>Location</h2>
+      <h3>Estes Park, CO</h3>
+      <p>There are several ways to get to Estes Park:</p>
+      <h4>Rent a Car</h4>
+      <p>Boulder, CO is a a good halfway point to grab food and do other shopping.</p>
+      <ul>
+        <li><a href="https://priceline.com">Priceline.com</a></li>
+        <li><a href="https://kayak.com">Kayak.com</a></li>
+        <li><a href="https://carrentals.com">CarRentals.com</a></li>
+      </ul>
+
+      <h4>Ride Share</h4>
+      <ul>
+        <li><a href="https://www.lyft.com/rider/airports/den">Lyft</a></li>
+        <li><a href="https://www.uber.com/global/en/airports/den/">Uber</a></li>
+      </ul>
+
+      <h4>Shuttle</h4>
+      <ul>
+        <li><a href="https://estesparkshuttle.com">Estes Park Shuttle</a></li>
+        <li><a href="https://denversairporttransportation.com">denversairporttransportation.com</a></li>
+        <li><a href="https://towncartoairportdenver.com">towncartoairportdenver.com</a></li>
+      </ul>
+
     </Card>
 
     <Card>
-      <h2>Ceremony</h2>
-      <p>The ceremony and reception will be held in Estes Park, CO. There are several ways to get to Estes:</p>
-      <ul>
-        <li>Rent a car</li>
-        <li>Uber</li>
-        <li>Airport Shuttle</li>
-      </ul>
+      <h2>Rehearsal Afternoon</h2>
+      <p>TBD</p>
     </Card>
 
     <Card>
@@ -40,9 +57,31 @@
     </Card>
 
     <Card>
+      <h2>Food</h2>
+      <p>Chef Grant from GB Culinary will prepare a four course Farm-to-table menu</p>
+    </Card>
+
+    <Card>
       <h2>Entertainment</h2>
       <p>Bring a boardgame to play!</p>
       <p>We're planning to have a billiards table and a hot tub, so bring a bathing suit!</p>
+    </Card>
+
+    <Card>
+      <h2>Lodging</h2>
+      <p>Estes is not a big town.  Anywhere you can find lodging is fine.</p>
+      <h3>Suggestions</h3>
+      <ul>
+        <li>Historic Crag's Lodge</li>
+        <li>The Ridgeline Hotel Estes Park</li>
+        <li>The Stanley Hotel</li>
+        <li>AirBnB / Homeaway</li>
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Local Activities</h2>
+      
     </Card>
 
   </div>
@@ -61,5 +100,9 @@ export default {
 <style>
 .venue {
   max-width: 300px;
+}
+
+ul {
+  margin-bottom: 16px;
 }
 </style>
