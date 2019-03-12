be604e8bed30e2d16f2617650493b46d329b118d
Blaine Price <1wbprice@gmail.com>
2019-03-12 19:48:16 -0400

    Update image

--- serverless-wedding-frontend/static/sw.js
+++ serverless-wedding-frontend/static/sw.js
@@ -10,32 +10,32 @@ workbox.precaching.precacheAndRoute([
     "revision": "9bba52f6a2a53a71e17bebc0a5b5cf97"
   },
   {
+    "url": "/_nuxt/19be65423300e5bab13e.js",
+    "revision": "28a406eb1559dbdc2f94ffbcceda6a32"
+  },
+  {
     "url": "/_nuxt/1b0e98eb5d802646634a.js",
     "revision": "61f0c7b5d38ac20ca52543844985fc53"
   },
   {
-    "url": "/_nuxt/23e6c0a19ca8b3e8021c.js",
-    "revision": "d63084c6773a7467d8ead9a479dbfc1f"
+    "url": "/_nuxt/285ebf9ee8854dc8df2b.js",
+    "revision": "f6813a0768869f9d931eb42578463481"
   },
   {
     "url": "/_nuxt/74d190a7481ac44ecd90.js",
     "revision": "e023dd8293a0daee574f5e2130b1696e"
   },
   {
+    "url": "/_nuxt/7ec6042752a36d4c780b.js",
+    "revision": "f6af6779e5785557bc9909b742c6e3f4"
+  },
+  {
     "url": "/_nuxt/8384488c203e07621cc5.js",
     "revision": "b9d88c9c94f3f4d66eb32c0059d5bef1"
   },
   {
-    "url": "/_nuxt/97e8b54229b7346c5bd8.js",
-    "revision": "5e2795120ead8c15434d55fa62af2e33"
-  },
-  {
     "url": "/_nuxt/a121df30b02755bc2285.js",
     "revision": "11f837787a1b8b38626d857b016682bc"
-  },
-  {
-    "url": "/_nuxt/a465f6333dc51092db07.js",
-    "revision": "75cb8d62095a0439ecfc9923e05a1d2f"
   },
   {
     "url": "/_nuxt/bbaa6332a187b8876ee7.js",
