29a873cacefa912e8277b65d099b052bf9c3bacc
Blaine Price <1wbprice@gmail.com>
2019-03-12 23:51:36 -0400

    Log record changes

--- serverless-wedding-rsvp-notifications/handler.py
+++ serverless-wedding-rsvp-notifications/handler.py
@@ -1,7 +1,18 @@
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+boto3.resource('dynamodb')
+deserializer = boto3.dynamodb.types.TypeDeserializer()
+
+def deserialize_dynamodb_record(record):
+   """
+   Uses Boto3 to deserialize a DynamoDB record into a Python dict
+   """
+
+   return {key: deserializer.deserialize(value) for key, value in record.copy().items()}
 
 
 def send_rsvp_notification(event, context):
@@ -11,7 +22,9 @@ def send_rsvp_notification(event, context):
 
     for record in event["Records"]:
 
-        logger.info("Received a record change event with shape: {}".format(record))
+        record_dictionary = deserialize_dynamodb_record(record["dynamodb"]["NewImage"])
+
+        logger.info("Received a record change event with shape: {}".format(record_dictionary))
 
         # If this isn't a modify event, quit the loop early
         if record["eventName"] != "MODFIY":
