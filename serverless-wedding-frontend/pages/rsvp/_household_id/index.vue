1f9e72d96cc23d4aca0349920654f6cfa6b92f0b
Blaine Price <1wbprice@gmail.com>
2019-04-15 21:24:59 -0400

    Update date (#54)

--- serverless-wedding-frontend/pages/rsvp/_household_id/index.vue
+++ serverless-wedding-frontend/pages/rsvp/_household_id/index.vue
@@ -6,7 +6,7 @@
 
       <header>
         <h2>We want to have you at our wedding!</h2>
-        <p>Please reply by <em>April 15th, 2019</em></p>
+        <p>Please reply by <em>April 22nd, 2019</em></p>
       </header>
 
       <RSVPCard
